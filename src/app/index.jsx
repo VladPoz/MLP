1938e45ed79b7820793ad0828ee552ecf2f7ee4b
229kab <229kab@kpvk.loc>
2026-01-06 13:11:35 +0500

    3

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,20 +1,39 @@
-import React from "react";
-import Lection from "./lection";
-import Practice from "./practice";
+import React, {useRef} from "react";
+import Right from "../img/icon/right.svg";
+import Left from "../img/icon/left.svg"
 import { Link } from "react-router-dom";
 
-function index(){
-    return(
-        <div className={"main"}>
-            <div className={"slider"}>
-                <div className={"slide b"}>
-                    <Link to={'/lection'}>
-                        <div className="slide__element">Lection</div>
+function Index() {
+    const sliderRef = useRef();
+    function SlideRight() {
+        sliderRef.current.scrollBy({
+            left: 100,
+        });
+    }
+
+    function SlideLeft() {
+        sliderRef.current.scrollBy({
+            left: -100,
+        });
+    }
+
+    return (
+        <div className="main">
+            <div className="slider" ref={sliderRef}>
+                <div className="slide b">
+                    <button className="slider__btn right" onClick={SlideRight}>
+                        <img className="icon" src={Right} />
+                    </button>
+                    <Link to="/lection">
+                        <div className="slide__element"><h3>Lection</h3></div>
                     </Link>
                 </div>
-                <div className={"slide r"}>
-                    <Link to={'/practice'}>.
-                        <div className="slide__element">Practice</div>
+                <div className="slide r">
+                    <button className="slider__btn left" onClick={SlideLeft}>
+                        <img className="icon" src={Left} />
+                    </button>
+                    <Link to="/practice">
+                        <div className="slide__element"><h3>Practice</h3></div>
                     </Link>
                 </div>
             </div>
@@ -22,4 +41,4 @@ function index(){
     );
 }
 
-export default index;
+export default Index;
--- src/app/lection.jsx
+++ src/app/lection.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, {useEffect} from "react";
+import LectionStyle from "../css/lection.css"
 import { Link } from "react-router-dom";
 
 function lection(){
+    
+    fetch('../data/data.json')
+    .then((response) => {response.json()})
+    .then((data) => {
+        console.log(data);   
+    })
+    
     return(
         <Link to={'/'}>Back</Link>
     )
