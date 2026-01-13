deb0a0a9dabd6fe650de46bb0b46432ca015b777
229kab <229kab@kpvk.loc>
2026-01-13 12:20:42 +0500

    6

--- src/app/lectionPage.jsx
+++ src/app/lectionPage.jsx
@@ -1,27 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useParams } from "react-router-dom";
 import { Document, Page, pdfjs } from 'react-pdf';
+import 'react-pdf/dist/Page/AnnotationLayer.css';
+import 'react-pdf/dist/Page/TextLayer.css';
 
-// Используем локальный worker из public
-pdfjs.GlobalWorkerOptions.workerSrc = '/pdf.worker.min.js';
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
 
-const LectionPage = () => {
+function LectionPage() {
+  const [link, setLink] = useState(null);
   const [numPages, setNumPages] = useState(null);
+  const { id } = useParams();
 
-  const onDocumentLoadSuccess = ({ numPages }) => setNumPages(numPages);
+  useEffect(() => {
+    fetch('http://localhost:3001/data')
+      .then(response => response.json())
+      .then(data => {
+        setLink(data[0].lecture[id].link);
+      });
+  }, [id]);
+
+  const onDocumentLoadSuccess = ({ numPages }) => {
+    setNumPages(numPages);
+  };
 
   return (
-    <div>
-      <h2>Лекция 1</h2>
-      <Document
-        file="../data/lecture/lecture_1/Лекция 1.pdf" // PDF в public
-        onLoadSuccess={onDocumentLoadSuccess}
-      >
-        {Array.from(new Array(numPages), (_, index) => (
-          <Page key={index + 1} pageNumber={index + 1} width={800} />
-        ))}
-      </Document>
+    <div className="lection__contents">
+      <div className="menu"></div>
+        <Document
+          file={link}
+          className="lection__document"
+          onLoadSuccess={onDocumentLoadSuccess}
+        >
+          {Array.from(new Array(numPages), (_, index) => (
+            <Page
+              pageNumber={index + 1}
+              renderTextLayer={false}
+            />
+          ))}
+        </Document>
     </div>
   );
-};
+}
 
 export default LectionPage;
